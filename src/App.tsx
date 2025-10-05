280f22929bec9722078b99e782e48e73ab0f3a62
André Aguiar <andre.aguiar522@gmail.com>
2025-10-05 20:01:53 -0300

    Criando o menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './styles/theme.css'
 import './styles/global.css'
 import {Container} from './components/Container';
-import {Heading} from './components/Heading';
 import {Logo} from "./components/Logo";
+import {Menu} from "./components/Menu";
 
 export function App() {
     return (
@@ -11,9 +11,7 @@ export function App() {
               <Logo/>
             </Container>
           <Container>
-            <Heading>
-              MENU
-            </Heading>
+            <Menu/>
           </Container>
         </>
     );
