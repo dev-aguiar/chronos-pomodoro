7b849d5cff180b17e01776d598c94bf06131715a
André Aguiar <andre.aguiar522@gmail.com>
2025-10-05 21:12:30 -0300

    Ajustando o form e input

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Container} from './components/Container';
 import {Logo} from "./components/Logo";
 import {Menu} from "./components/Menu";
 import {Countdown} from "./components/Countdown";
+import {Input} from "./components/Input";
 
 export function App() {
     return (
@@ -11,11 +12,39 @@ export function App() {
             <Container>
               <Logo/>
             </Container>
+
           <Container>
             <Menu/>
           </Container>
+
           <Container>
             <Countdown/>
+          </Container>
+
+          <Container>
+            <form className='form' action="">
+              <div className='formRow'>
+                <Input
+                  labelText='Task:'
+                  id='task'
+                  type='text'
+                  placeholder='Digite a sua task...'
+                />
+              </div>
+
+              <div className='formRow'>
+                <p>Lorem</p>
+              </div>
+
+              <div className='formRow'>
+                <p>Ciclos</p>
+                <p>0 0 0 0 0 </p>
+              </div>
+
+              <div className='formRow'>
+                <button>Enviar</button>
+              </div>
+            </form>
           </Container>
         </>
     );
